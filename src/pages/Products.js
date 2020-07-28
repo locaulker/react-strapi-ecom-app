8f13925b9a53fb22c9d720706f7750265166be87
Lawrence <Caulker>
2020-07-28 18:08:14 -0400

    added product component

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,7 +5,6 @@ import ProductList from "../components/Products/ProductList"
 
 export default function Products() {
   const { loading, products } = React.useContext(ProductContext)
-  console.log(products)
 
   if (loading) {
     return <Loading />
