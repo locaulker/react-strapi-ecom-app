59ca4640404d05380177d317190b02522e6ed812
Lawrence <locaulker@gmail.com>
2020-07-28 01:47:04 -0400

    setup context api and installed axios

--- src/context/products.js
+++ src/context/products.js
@@ -1,1 +1,18 @@
-// products context
+import React from "react"
+
+export const ProductContext = React.createContext()
+
+// Provider, Consumer, useContext()
+const ProductProvider = ({ children }) => {
+  const [loading, setLoading] = React.useState(false)
+  const [products, setProducts] = React.useState([])
+  const [featured, setFeatured] = React.useState([])
+
+  return (
+    <ProductContext.Provider value={{ loading, products, featured }}>
+      {children}
+    </ProductContext.Provider>
+  )
+}
+
+export default ProductProvider
