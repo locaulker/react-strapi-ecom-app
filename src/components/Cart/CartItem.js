7006a6539e4f5edb6ac662a6513ef42ae24bee78
Lawrence <locaulker@gmail.com>
2020-07-30 00:20:20 -0400

    added cart items

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React from "react"
+import { FaAngleUp, FaAngleDown } from "react-icons/fa"
+import { CartContext } from "../../context/cart.context"
 
-export default function CartItem() {
-  return <h1>hello form cart item</h1>;
+const CartItem = ({ id, image, title, price, quantity }) => {
+  // cart context
+
+  return (
+    <article className="cart-item">
+      <img src={image} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h5>${price}</h5>
+        <button
+          className="cart-btn remove-btn"
+          onClick={() => {
+            console.log("Item Removed")
+          }}
+        >
+          Remove
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="cart-btn quantity-btn"
+          onClick={() => {
+            console.log("qty increase")
+          }}
+        >
+          <FaAngleUp />
+        </button>
+        <p className="item-quantity">{quantity}</p>
+        <button
+          type="button"
+          className="cart-btn quantity-btn"
+          onClick={() => {
+            console.log("qty decrease")
+          }}
+        >
+          <FaAngleDown />
+        </button>
+      </div>
+    </article>
+  )
 }
+
+export default CartItem
